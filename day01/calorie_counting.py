664225967871887c358af1055a7e418ae82d8944
donatoaz <donatoaz@amazon.com>
2022-12-01 17:55:39 -0300

    day 2 part 2

--- day01/calorie_counting.py
+++ day01/calorie_counting.py
@@ -1,9 +1,18 @@
 
 
 
-def part_1(input: str):
+def part_1(input: str) -> int:
     elven_meals = [map(int, calories.split("\n")) for calories in input.strip().split("\n\n")]
 
     total_cals_per_elf = [sum(calories) for calories in elven_meals]
 
     return max(total_cals_per_elf) 
+
+def part_2(input: str) -> int:
+    elven_meals = [map(int, calories.split("\n")) for calories in input.strip().split("\n\n")]
+
+    total_cals_per_elf = [sum(calories) for calories in elven_meals]
+
+    sorted_total_cals_per_elf = sorted(total_cals_per_elf, reverse=True)
+
+    return sum(sorted_total_cals_per_elf[:3])
--- test/day01/calorie_counting_test.py
+++ test/day01/calorie_counting_test.py
@@ -1,10 +1,10 @@
 import pytest
 import os
 
-from day01.calorie_counting import part_1
+from day01.calorie_counting import part_1, part_2
 
 @pytest.fixture
-def sample_input_1():
+def sample_input():
     return """1000
 2000
 3000
@@ -26,8 +26,14 @@ def my_input():
     with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r', encoding="utf8") as f:
         return f.read()
 
-def test_part_1_sample(sample_input_1):
-    assert part_1(sample_input_1) == 24_000
+def test_part_1_sample(sample_input):
+    assert part_1(sample_input) == 24_000
 
 def test_part_1(my_input):
     assert part_1(my_input) == 71_124
+
+def test_part_2_sample(sample_input):
+    assert part_2(sample_input) == 45_000
+
+def test_part_2(my_input):
+    assert part_2(my_input) == 204_639
